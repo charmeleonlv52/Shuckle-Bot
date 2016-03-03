e06a7d796170168589f83df01a8d541a83f7e05a
Andrew Lee <andrew.dragon.lee@gmail.com>
2016-03-02 21:34:38 -0500

    owner help

--- bots/owner.py
+++ bots/owner.py
@@ -4,11 +4,28 @@ from shuckle.command import command
 from shuckle.error import ShuckleUserPermissionError
 import sys
 
+HELP = """
+__Owner Commands:__:
+
+Restarts Shuckle:
+```
+@{bot_name} owner restart
+```
+Reload all modules:
+```
+@{bot_name} owner reload
+```
+"""
+
 class OwnerBot(object):
     __group__ = 'owner'
 
     def __init__(self, client):
         self.client = client
+
+    @command()
+    async def help(self, message):
+        await self.client.say(HELP.strip().format(bot_name=self.client.user.name))
 
     @command()
     async def restart(self, message):
