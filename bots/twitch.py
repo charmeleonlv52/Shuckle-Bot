6bd6d00e0fa9e14af3c195362465717c1654f578
Andrew Lee <andrew.dragon.lee@gmail.com>
2016-03-03 22:29:42 -0500

    fixing

--- bots/twitch.py
+++ bots/twitch.py
@@ -13,11 +13,7 @@ class TwitchBot(object):
         self.client = client
         self.to_check = {}
 
-<<<<<<< Updated upstream
     def setup(self):
-=======
-    async def setup(self):
->>>>>>> Stashed changes
         '''
         for each stream to check:
             if streaming:
